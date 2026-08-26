c87ba6c30cf81723a75826aa88fabc4ccc874106
402-20-bhumphandin <54355@yupparaj.ac.th>
2026-08-26 15:16:50 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -13,6 +13,7 @@ if "ans1_val" not in st.session_state:
 if "ans2_val" not in st.session_state:
     st.session_state.ans4_val = ""
 
+
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
@@ -21,6 +22,8 @@ def reset_game():
     st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
+
+
 
 
 # ----------------------------------------------------
